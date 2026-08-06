88f1400c03879eed470e590ca89e1ef2f5ee4574
swaroopufs <swaroop@ufstechnologies.in>
2026-08-06 16:40:25 +0530

    db lead, sp lead filter, filter ui

--- frontend/src/app/components/lead-filter-dropdown/lead-filter-dropdown.component.ts
+++ frontend/src/app/components/lead-filter-dropdown/lead-filter-dropdown.component.ts
@@ -1,57 +1,118 @@
-import { Component, Input, Output, EventEmitter, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ElementRef, HostListener, ViewChild, OnInit, OnDestroy } from '@angular/core';
+import { Subject, Subscription } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { Lead_Service } from '../../services/Lead.Service';
 
 @Component({
   selector: 'app-lead-filter-dropdown',
   templateUrl: './lead-filter-dropdown.component.html',
   styleUrls: ['./lead-filter-dropdown.component.css']
 })
-export class LeadFilterDropdownComponent {
-  @Input() options: string[] = [];
+export class LeadFilterDropdownComponent implements OnInit, OnDestroy {
+  @Input() type: string = '';
   @Input() placeholder: string = 'Select...';
-  @Output() selectionChange = new EventEmitter<string>();
+  @Output() selectionChange = new EventEmitter<any>();
 
   @ViewChild('searchInput', { static: false }) searchInput: any;
 
   isOpen = false;
   searchTerm = '';
-  selectedOption = '';
+  selectedOptionName = '';
+  options: any[] = [];
+  
+  page = 1;
+  isLoading = false;
+  hasMore = true;
 
-  constructor(private eRef: ElementRef) {}
+  private searchSubject = new Subject<string>();
+  private searchSubscription!: Subscription;
 
-  get filteredOptions() {
-    if (!this.searchTerm) return this.options;
-    return this.options.filter(opt => opt.toLowerCase().includes(this.searchTerm.toLowerCase()));
+  constructor(private eRef: ElementRef, private leadService: Lead_Service) {}
+
+  ngOnInit() {
+    this.searchSubscription = this.searchSubject.pipe(
+      debounceTime(500),
+      distinctUntilChanged()
+    ).subscribe(searchValue => {
+      this.searchTerm = searchValue;
+      this.loadData(true);
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.searchSubscription) {
+      this.searchSubscription.unsubscribe();
+    }
+  }
+
+  loadData(reset: boolean = false) {
+    if (reset) {
+      this.page = 1;
+      this.options = [];
+      this.hasMore = true;
+    }
+
+    if (!this.hasMore || this.isLoading || !this.type) return;
+
+    this.isLoading = true;
+    this.leadService.Get_Lead_Filter_Dropdown(this.type, this.searchTerm, this.page)
+      .subscribe((res: any[]) => {
+        if (res && res.length > 0) {
+          this.options = [...this.options, ...res];
+          if (res.length < 20) {
+            this.hasMore = false; // Less than 20 means we hit the end
+          }
+        } else {
+          this.hasMore = false;
+        }
+        this.isLoading = false;
+      }, err => {
+        console.error(err);
+        this.isLoading = false;
+      });
   }
 
   toggleDropdown(event: Event) {
     this.isOpen = !this.isOpen;
     if (this.isOpen) {
+      if (this.options.length === 0) {
+        this.loadData(true);
+      }
       setTimeout(() => {
         if (this.searchInput) {
           this.searchInput.nativeElement.focus();
         }
       });
     } else {
-      this.searchTerm = '';
+      // Keep search term but don't clear options on close unless you want to
     }
   }
 
-  selectOption(option: string, event: Event) {
-    this.selectedOption = option;
+  selectOption(option: any, event: Event) {
+    this.selectedOptionName = option.name;
     this.selectionChange.emit(option);
     this.isOpen = false;
-    this.searchTerm = '';
   }
 
   onSearch(event: any) {
-    this.searchTerm = event.target.value;
+    this.searchSubject.next(event.target.value);
+  }
+  
+  onScroll(event: any) {
+    const element = event.target;
+    // When scroll reaches within 10px of bottom, load more
+    if (element.scrollHeight - element.scrollTop <= element.clientHeight + 10) {
+      if (!this.isLoading && this.hasMore) {
+        this.page++;
+        this.loadData(false);
+      }
+    }
   }
 
   @HostListener('document:click', ['$event'])
   clickout(event: any) {
     if (this.isOpen && !this.eRef.nativeElement.contains(event.target)) {
       this.isOpen = false;
-      this.searchTerm = '';
     }
   }
 }
